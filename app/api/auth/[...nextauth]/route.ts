d9d5c0ebdd169a9634cdc12e755d0e3e1b3689c3
harshdalmia <harshdalmia08@gmail.com>
2025-08-07 22:44:23 +0530

    should work now

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,30 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/app/lib/auth";
 
-const handler = NextAuth(authOptions);
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prismaClient } from "@/app/lib/db";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
